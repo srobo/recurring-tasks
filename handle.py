974a263c5422a75aafe731f30ff52406ae984053
Alistair Lynn <arplynn@gmail.com>
2014-11-07 15:29:58 +0000

    Support creating tickets through handle.py

--- handle.py
+++ handle.py
@@ -4,6 +4,8 @@ import yaml
 from collections import namedtuple
 from termcolor import cprint
 import textwrap
+import urllib.parse
+from getpass import getpass
 
 ROOT = Path(__file__).parent
 
@@ -34,7 +36,7 @@ def trac_description_text(ticket):
 
 class FakeTrac(object):
     def __init__(self):
-        self.next_ticket = 2501
+        self.next_ticket = 2673
         self._known_titles = {}
 
     def submit(self, ticket):
@@ -56,7 +58,51 @@ class FakeTrac(object):
     def title(self, ticket_number):
         return self._known_titles.get(ticket_number, '')
 
-TRAC = FakeTrac()
+class RealTrac(object):
+    def __init__(self, root):
+        self.root = root
+        import xmlrpc.client as xml
+        attrs = urllib.parse.urlsplit(root)
+        username = input('SR username: ')
+        password = getpass('SR password: ')
+        generated_netloc = '{}:{}@{}{}'.format(urllib.parse.quote(username),
+                                               urllib.parse.quote(password),
+                                               attrs.hostname,
+                                               ':{}'.format(attrs.port)
+                                                 if attrs.port is not None
+                                                 else '')
+        generated_parts = (attrs.scheme,
+                           generated_netloc,
+                           attrs.path.rstrip('/') + '/login/xmlrpc',
+                           '', '')
+        target_url = urllib.parse.urlunsplit(generated_parts)
+        self._xml = xml.ServerProxy(target_url)
+        print(self._xml.system.methodHelp('ticket.create'))
+
+    def submit(self, ticket):
+        desc = trac_description_text(ticket)
+        attrs = {}
+        if ticket.component is not None:
+            attrs['component'] = ticket.component
+        if ticket.milestone is not None:
+            attrs['milestone'] = ticket.milestone
+        attrs['priority'] = ticket.priority
+        ticket_number = self._xml.ticket.create(ticket.summary,
+                                                desc,
+                                                attrs,
+                                                False)
+        print('Created ticket #{}: {}'.format(ticket_number,
+                                              ticket.summary))
+        return ticket_number
+
+    def title(self, ticket_number):
+        ticket_data = self._xml.ticket.get(ticket_number)
+        return ticket_data[3]['summary']
+
+if arguments.trac_root is not None:
+    TRAC = RealTrac(arguments.trac_root)
+else:
+    TRAC = FakeTrac()
 
 def add(element):
     if element in elements:
@@ -76,6 +122,9 @@ def handle_dep(key):
         return key
     return add(key)
 
+COMPONENTS = ('Arena', 'Competition', 'Website', 'sysadmin',
+              'Rules', 'Tall Ship')
+
 def process(path):
     with path.open('r') as f:
         data = f.read()
@@ -90,7 +139,7 @@ def process(path):
     priority = raw_elements.get('priority', 'major')
     if priority not in ('trivial', 'minor', 'major', 'critical', 'blocker'):
         raise RuntimeError('{} has an invalid priority: {}'.format(path, priority))
-    if component not in ('Arena', 'Competition', 'Website', 'sysadmin', 'Rules'):
+    if component not in COMPONENTS:
         raise RuntimeError('{} has an unknown component: {}'.format(path, component))
     milestone = raw_elements.get('milestone')
     dependencies = raw_elements.get('dependencies', ())
