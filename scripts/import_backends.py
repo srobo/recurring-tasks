197bfcc04223b19a8a08038f94232b2e1387b62a
Peter Law <PeterJCLaw@gmail.com>
2019-09-08 16:06:12 +0100

    Initial support for GitHub import using PyGithub

--- scripts/import_backends.py
+++ scripts/import_backends.py
@@ -1,8 +1,10 @@
+import pathlib
 import textwrap
 import urllib.parse
 from getpass import getpass
 from typing import TYPE_CHECKING, Dict
 
+import github  # type: ignore
 from termcolor import cprint
 from ticket_type import Ticket
 
@@ -104,3 +106,80 @@ class RealTracBackend:
     def title(self, ticket_number: int) -> str:
         ticket_data = self._xml.ticket.get(ticket_number)
         return ticket_data[3]['summary']  # type: ignore
+
+
+def get_github_credential() -> str:
+    config_file = pathlib.Path(__file__).parent.parent / '.github-auth'
+
+    auth_token = None
+
+    if config_file.exists():
+        with config_file.open() as f:
+            auth_token = f.read()
+
+    if auth_token is None:
+        auth_token = getpass("GitHub Auth Token: ")
+
+        store = input("Store auth token? [y/N]: ")
+        if store in ('Y', 'y'):
+            with config_file.open(mode='w') as f:
+                f.write(auth_token)
+
+    return auth_token
+
+
+class GitHubBackend:
+    def __init__(self, repo_name: str) -> None:
+        self.github = github.Github(get_github_credential())
+        self.repo = self.github.get_repo(repo_name)
+        self.milestones = {x.title: x for x in self.repo.get_milestones()}
+
+        self._known_titles: Dict[int, str] = {}
+
+    @staticmethod
+    def _section(heading: str, body: str) -> str:
+        return f"\n\n### {heading}:\n\n{body}"
+
+    def _original_link(self, ticket: Ticket) -> str:
+        url = urllib.parse.urljoin(
+            self.repo.url,
+            f'blob/master/{ticket.original_name}.yaml',
+        )
+        return f"[{ticket.original_name}]({url})"
+
+    def description_text(self, ticket: Ticket) -> str:
+        text = ticket.description
+
+        text += self._section("Original", self._original_link(ticket))
+
+        if ticket.dependencies:
+            text += self._section("Dependencies", "\n".join(
+                f" * #{dep} {self.title(dep)}".rstrip()
+                for dep in sorted(ticket.dependencies)
+            ))
+
+        return text.strip()
+
+    def get_or_create_milestone(self, title: str) -> github.Milestone:
+        try:
+            return self.milestones[title]
+        except KeyError:
+            print(f"Creating Milestong {title}")
+            milestone = self.repo.create_milestone(title)
+            self.milestones[title] = milestone
+            return milestone
+
+    def submit(self, ticket: Ticket) -> int:
+        issue = self.repo.create_issue(
+            ticket.summary,
+            self.description_text(ticket),
+            milestone=self.get_or_create_milestone(ticket.milestone),
+            # TODO: priority & component
+        )
+
+        ticket_number: int = issue.id
+        self._known_titles[ticket_number] = ticket.summary
+        return ticket_number
+
+    def title(self, ticket_number: int) -> str:
+        return self._known_titles.get(ticket_number, '')
